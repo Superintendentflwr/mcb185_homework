28fb336486ec20a0c22fb68f4aff66ad8e052172
Superintendentflwr <higginsmaisey@gmail.com>
2026-06-27 19:51:00 -0700

    now

---
+++ 02status.sh
@@ -0,0 +1,11 @@
+# Report the date
+date 
+# Reports the full name of your flavor of Unix
+uname -a
+# Reports your environment variables 
+printenv
+# Reports the version of Python you have installed 
+python3 --version
+# Reports the directories and files in your Code directory
+ls -R /mnt/c/Users/higgi/OneDrive/Desktop/github_practice/Code
+
